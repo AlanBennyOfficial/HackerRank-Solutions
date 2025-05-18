1c0935e5a7616e69c295d17977f4783294901459
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-05-18 16:18:23 +0530

    Modified files via git

--- Sem2-Lab_Exam/06.Alphabet Rangoli.py
+++ Sem2-Lab_Exam/06.Alphabet Rangoli.py
@@ -1,15 +1,19 @@
 def print_rangoli(size):
     # code
-    x=[]
+    rangoli=[]
     for i in range(size,0,-1):
-        l=[]
+        row_letters=[] 
+
         for j in range(size,i-1,-1):
-            l.append(chr((j-1)+97))
-        if len(l)>1:
-            l=l+l[-2::-1]
-        x.append(("-".join(l).center((2*(2*size-1)-1),'-')))
-    x=x+x[-2::-1]
-    for b in x:
+            row_letters.append(chr((j-1)+97))
+
+        if len(row_letters)>1:
+            row_letters=row_letters+row_letters[-2::-1]
+            
+        rangoli.append(("-".join(row_letters).center((2*(2*size-1)-1),'-')))
+
+    fullrangoli=rangoli+rangoli[-2::-1]
+    for b in fullrangoli:
         print(b)
 
 if __name__ == '__main__':
